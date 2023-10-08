17dd7f629dd2ded45825cc2912075681c7f5af36
dennis <76782124@certus.edu.pe>
2023-10-08 18:52:09 +0200

    PrimerCommit

---
+++ src/main/java/com/zinespace/autocines/AutocinesApplication.java
@@ -0,0 +1,13 @@
+package com.zinespace.autocines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zinespace/autocines/AutocinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinespace.autocines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
